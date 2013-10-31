898d591aef32d685053c50295d0dcfed91ff07bf
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2013-10-31 12:17:19 +0100

    Checkstyle Listener created

---
+++ sonar-checkstyle-processor/src/main/java/de/alexanderlindhorst/sonarcheckstyleprocessor/DefaultAuditListener.java
@@ -0,0 +1,45 @@
+package de.alexanderlindhorst.sonarcheckstyleprocessor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
+
+/**
+ * @author lindhrst (original author)
+ */
+public class DefaultAuditListener implements AuditListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultAuditListener.class);
+
+    @Override
+    public void auditStarted(AuditEvent auditEvent) {
+        LOGGER.debug("auditStarted called for AuditEvent "+auditEvent);
+    }
+
+    @Override
+    public void auditFinished(AuditEvent auditEvent) {
+        LOGGER.debug("auditFinished called for AuditEvent "+auditEvent);
+    }
+
+    @Override
+    public void fileStarted(AuditEvent auditEvent) {
+        LOGGER.debug("fileStarted called for AuditEvent "+auditEvent);
+    }
+
+    @Override
+    public void fileFinished(AuditEvent auditEvent) {
+        LOGGER.debug("fileFinished called for AuditEvent "+auditEvent);
+    }
+
+    @Override
+    public void addError(AuditEvent auditEvent) {
+        LOGGER.debug("addError called for AuditEvent "+auditEvent);
+    }
+
+    @Override
+    public void addException(AuditEvent auditEvent, Throwable thrwbl) {
+        LOGGER.debug("addException called for AuditEvent "+auditEvent);
+    }
+}
