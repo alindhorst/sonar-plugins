c24d3b275d7051c55e2950a4c60161bff83c4b9f
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2013-10-31 12:36:52 +0100

    Basic Plugin infrastructure in place; glue still missing, no real functionality.

---
+++ sonar-checkstyle-plugin/src/main/java/de/alexanderlindhorst/sonarcheckstyleplugin/SonarCheckstyleAnnotation.java
@@ -0,0 +1,19 @@
+package de.alexanderlindhorst.sonarcheckstyleplugin;
+
+import org.openide.text.Annotation;
+
+/**
+ * @author lindhrst (original author)
+ */
+public class SonarCheckstyleAnnotation extends Annotation {
+
+    @Override
+    public String getAnnotationType() {
+        return "de-alexanderlindhorst-sonarcheckstyleplugin-sonarcheckstyleannotation";
+    }
+
+    @Override
+    public String getShortDescription() {
+        return "This is a wannabe sonar/checkstyle issue";
+    }
+}
