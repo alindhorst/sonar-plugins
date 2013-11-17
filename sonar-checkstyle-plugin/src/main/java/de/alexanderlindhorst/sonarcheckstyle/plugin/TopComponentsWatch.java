669e1be3e9ce478db0c688d6f2d8ee4f0c5af804
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2013-11-17 02:01:49 +0100

    Milestone: Annotation visible for first time (no real checkstyle behind that yet, but ...)

--- sonar-checkstyle-plugin/src/main/java/de/alexanderlindhorst/sonarcheckstyle/plugin/TopComponentsWatch.java
+++ sonar-checkstyle-plugin/src/main/java/de/alexanderlindhorst/sonarcheckstyle/plugin/TopComponentsWatch.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javax.swing.JOptionPane;
 
+import org.openide.filesystems.FileEvent;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObject;
 import org.openide.modules.ModuleInstall;
@@ -17,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
+import de.alexanderlindhorst.sonarcheckstyle.plugin.gui.SonarCheckstyleDocumentGuiHelper;
 
 /**
  * Hooks itself up with WindowManager upon module start and registers listeners. Thus, will be notified of any change in
@@ -68,6 +70,9 @@ public class TopComponentsWatch implements Runnable {
                 }
                 FileObject file = dataObject.getPrimaryFile();
                 LOGGER.debug("Found newly opened filed {}", file);
+                SonarCheckstyleDocumentGuiHelper helper = new SonarCheckstyleDocumentGuiHelper();
+                file.addFileChangeListener(helper);
+                helper.fileChanged(new FileEvent(file));
             }
         }
     }
