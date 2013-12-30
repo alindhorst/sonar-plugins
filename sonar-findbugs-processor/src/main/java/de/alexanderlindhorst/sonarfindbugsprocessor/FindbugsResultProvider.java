431bf7766fa165895eda654db29c75d4121904e9
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2013-12-30 09:00:47 +0100

    Empty file shells to keep project structure in GIT

---
+++ sonar-findbugs-processor/src/main/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProvider.java
@@ -0,0 +1,9 @@
+package de.alexanderlindhorst.sonarfindbugsprocessor;
+
+public class FindbugsResultProvider implements Runnable {
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ sonar-findbugs-processor/src/test/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProviderTest.java
@@ -0,0 +1,4 @@
+package de.alexanderlindhorst.sonarfindbugsprocessor;
+
+public class FindbugsResultProviderTest {
+}
