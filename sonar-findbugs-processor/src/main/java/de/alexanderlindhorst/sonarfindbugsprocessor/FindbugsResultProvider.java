bfc838d32b60414a2e564e3d0d36131305362696
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2014-02-16 23:04:27 +0100

    First shot at FindBugs

--- sonar-findbugs-processor/src/main/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProvider.java
+++ sonar-findbugs-processor/src/main/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProvider.java
@@ -1,9 +1,34 @@
 package de.alexanderlindhorst.sonarfindbugsprocessor;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.umd.cs.findbugs.FindBugs2;
+import edu.umd.cs.findbugs.Project;
+
 public class FindbugsResultProvider implements Runnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FindbugsResultProvider.class);
+    private Project project;
+    private FindBugs2 findbugs;
+    private File targetFile;
+
+    public FindbugsResultProvider(Project project, File targetFile) {
+        this.project = project;
+        findbugs = new FindBugs2();
+    }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            findbugs.execute();
+        } catch (IOException ex) {
+            LOGGER.error("Exception while processing file", ex);
+        } catch (InterruptedException ex) {
+            LOGGER.error("Exception while processing file", ex);
+        }
     }
 }
--- sonar-findbugs-processor/src/test/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProviderTest.java
+++ sonar-findbugs-processor/src/test/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProviderTest.java
@@ -1,4 +1,23 @@
 package de.alexanderlindhorst.sonarfindbugsprocessor;
 
+import java.io.File;
+import java.net.URL;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class FindbugsResultProviderTest {
+
+    private static URL testFileResourceURL;
+
+    @BeforeClass
+    public static void setUpClass() {
+        testFileResourceURL = FindbugsResultProvider.class.getResource("/TestFile.java");
+    }
+
+    @Test
+    public void findBugProjectGetsActivated() throws Exception {
+        FindbugsResultProvider instance = new FindbugsResultProvider(null, new File(testFileResourceURL.toURI()));
+        instance.run();
+    }
 }
