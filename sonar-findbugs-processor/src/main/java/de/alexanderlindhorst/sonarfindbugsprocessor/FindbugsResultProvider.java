996f58eb2d880de84964e9d982445bd2111640da
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2014-03-19 21:56:27 +0100

    Fooling around with FindBugs

--- sonar-findbugs-processor/src/main/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProvider.java
+++ sonar-findbugs-processor/src/main/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProvider.java
@@ -6,10 +6,17 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.umd.cs.findbugs.BugCollection;
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BugReporterObserver;
 import edu.umd.cs.findbugs.FindBugs2;
 import edu.umd.cs.findbugs.Project;
+import edu.umd.cs.findbugs.ProjectStats;
+import edu.umd.cs.findbugs.classfile.ClassDescriptor;
+import edu.umd.cs.findbugs.classfile.MethodDescriptor;
 
-public class FindbugsResultProvider implements Runnable {
+public class FindbugsResultProvider implements Runnable, BugReporter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FindbugsResultProvider.class);
     private Project project;
@@ -19,6 +26,7 @@ public class FindbugsResultProvider implements Runnable {
     public FindbugsResultProvider(Project project, File targetFile) {
         this.project = project;
         findbugs = new FindBugs2();
+        findbugs.setBugReporter(this);
     }
 
     @Override
@@ -30,5 +38,78 @@ public class FindbugsResultProvider implements Runnable {
         } catch (InterruptedException ex) {
             LOGGER.error("Exception while processing file", ex);
         }
+    }
+
+    @Override
+    public void setErrorVerbosity(int level) {
+        LOGGER.info("setErrorVerbosity({})", level);
+    }
+
+    @Override
+    public void setPriorityThreshold(int threshold) {
+        LOGGER.info("setPriorityThreshold({})", threshold);
+    }
+
+    @Override
+    public void reportBug(BugInstance bugInstance) {
+        LOGGER.error("Bug reported: {}", bugInstance);
+    }
+
+    @Override
+    public void finish() {
+        LOGGER.info("finish()");
+    }
+
+    @Override
+    public void reportQueuedErrors() {
+        LOGGER.info("reportQueuedErrors()");
+    }
+
+    @Override
+    public void addObserver(BugReporterObserver observer) {
+        LOGGER.info("addObserver({})", observer);
+    }
+
+    @Override
+    public ProjectStats getProjectStats() {
+        LOGGER.info("getProjectStats()");
+        ProjectStats projectStats = new ProjectStats();
+        return projectStats;
+    }
+
+    @Override
+    public BugCollection getBugCollection() {
+        LOGGER.info("getBugCollection()");
+        return null;
+    }
+
+    @Override
+    public void reportMissingClass(ClassNotFoundException ex) {
+        LOGGER.info("reportMissingClass({})", ex);
+    }
+
+    @Override
+    public void reportMissingClass(ClassDescriptor classDescriptor) {
+        LOGGER.info("reportMissingClass({})", classDescriptor);
+    }
+
+    @Override
+    public void logError(String message) {
+        LOGGER.error("logError({})", message);
+    }
+
+    @Override
+    public void logError(String message, Throwable e) {
+        LOGGER.error("logError()", e);
+    }
+
+    @Override
+    public void reportSkippedAnalysis(MethodDescriptor method) {
+        LOGGER.info("reportSkippedAnalysis({})", method);
+    }
+
+    @Override
+    public void observeClass(ClassDescriptor classDescriptor) {
+        LOGGER.info("observeClass({})", classDescriptor);
     }
 }
--- sonar-findbugs-processor/src/test/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProviderTest.java
+++ sonar-findbugs-processor/src/test/java/de/alexanderlindhorst/sonarfindbugsprocessor/FindbugsResultProviderTest.java
@@ -2,22 +2,43 @@ package de.alexanderlindhorst.sonarfindbugsprocessor;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Properties;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.umd.cs.findbugs.Project;
 
 public class FindbugsResultProviderTest {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(FindbugsResultProviderTest.class);
     private static URL testFileResourceURL;
+    private static URL configURL;
 
     @BeforeClass
     public static void setUpClass() {
         testFileResourceURL = FindbugsResultProvider.class.getResource("/TestFile.java");
+        configURL = FindbugsResultProvider.class.getResource("/findbugs-config.xml");
     }
 
     @Test
+    @Ignore
     public void findBugProjectGetsActivated() throws Exception {
-        FindbugsResultProvider instance = new FindbugsResultProvider(null, new File(testFileResourceURL.toURI()));
+        Properties properties = System.getProperties();
+        for (String propertyName : properties.stringPropertyNames()) {
+            LOGGER.info("{}: {}", propertyName, properties.get(propertyName));
+        }
+        LOGGER.info("URL: " + testFileResourceURL);
+        Project project = Project.readXML(new File(configURL.toURI()));
+        project.setProjectName("Testproject");
+        project.addSourceDir(System.getProperty("user.dir") + "/target/classes");
+        project.addAuxClasspathEntry(System.getProperty("user.dir") + "/target/classes");
+        project.addAuxClasspathEntry(new File(testFileResourceURL.toURI()).getParent());
+        LOGGER.info("Project: {}", project);
+        FindbugsResultProvider instance = new FindbugsResultProvider(project, new File(testFileResourceURL.toURI()));
         instance.run();
     }
 }
